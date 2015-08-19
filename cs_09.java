7c91263c8c8bf9bf22fa0fd074c369ee614f8452
zxcvwei <104021031@live.asia.edu.tw>
2015-08-19 14:35:08 +0800

    java12

--- cs_09.java
+++ cs_09.java
@@ -5,10 +5,10 @@ public class cs_09 {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		System.out.println("請輸入書號數字");
-		int a=scn.nextInt();
-		int num[] = new int [10];
+		long a=scn.nextLong();/*long存取的數字較大*/
+		int[] num = new int[10];
 		for(int i=9;i>=0;i--){
-			num[i]=a%10;
+			num[i]=(int) (a%10);/*long經過除法資料型態屬於int*/
 			a=a/10;
 		}
 		int x[] = {1,2,3,4,5,6,7,8,9,10};
@@ -16,11 +16,11 @@ public class cs_09 {
 		for(int i=0;i<10;i++){
 			sum[i]=num[i]*x[9-i];
 		}  單個算式存在陣列 要自己再加一次*/
-		int sum=0;
+		long sum=0;
 		for(int i=0;i<10;i++){
 			sum=sum+num[i]*x[9-i];
 		}
-		int f = sum%11;
+		long f = sum%11;
 		if(f==0){
 			System.out.print("ISBN書號正確");
 		}else{
